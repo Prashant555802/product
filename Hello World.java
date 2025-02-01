3a7e77a6d5ec396cff239a55969eddf8c2f6f508
Prashant555802 <prashantpatil7432@gmail.com>
2025-02-01 10:33:28 +0530

    Add files via upload

---
+++ Hello World.java
@@ -0,0 +1,6 @@
+public class p1{
+                             public static void main(String[] atgs){
+                                                       //TODO Auto-generated methordstub
+                                                       System.out.println("Hello World")
+                             }
+}
