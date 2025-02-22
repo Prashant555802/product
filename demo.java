08296159ec068c2dfa7b7d5a718287f4ca5325d9
Prashant555802 <prashantpatil7432@gmail.com>
2025-02-21 20:30:47 -0800

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,4 +1,4 @@
-public class p1 {
+ class p1 {
             public static void main(String[] args) {
                   //TODO Auto-generated method stub
                   System.out.println("HELLO WORLD");
